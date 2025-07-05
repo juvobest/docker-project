97b532059dac869b2404fffac7fbd72773c56434
Juvo <juvobest@gmail.com>
2025-07-05 16:49:32 +0100

    Fix: Updated index.html and force rebuild

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return "Hello from EC2 via Docker!"
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    return render_template('index.html')
